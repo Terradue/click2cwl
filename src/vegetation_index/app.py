6c1130314a03219ef9fa3065bd9407781c15b418
Simone Vaccari <simone.vaccari@terradue.com>
2020-11-25 16:44:37 +0100

    app.py and helper.py tested and edited with VIs

--- src/vegetation_index/app.py
+++ src/vegetation_index/app.py
@@ -7,8 +7,7 @@ import click
 from pystac import *
 from shapely.wkt import loads
 from time import sleep
-from .helpers import get_item, cog, set_env, get_assets, normalized_difference
-
+from .helpers import get_item, cog, set_env, get_assets, normalized_difference#, generate_evi, generate_savi, generate_msavi
 
 
 logging.basicConfig(stream=sys.stderr, 
@@ -30,62 +29,78 @@ aoi = dict([('id', 'aoi'),
 input_reference = dict([('id', 'input_reference'), 
                         ('label', 'EO product for vegetation index'),
                         ('doc', 'EO product for vegetation index'),
-                        ('value', '/workspace/application-chaining/68nl6_bz/s2-pre'), 
+                        ('value', '/workspace/application-chaining/68nl6_bz/s2-pre'), # this is created in the stage-in process
                         ('type', 'Directory'),
                         ('stac:collection', 'source'),
                         ('stac:href', 'catalog.json'),
                         ('scatter', 'True')])
 
-
 @click.command()
 @click.option('--input_reference', '-i', 'e_input_reference', help=input_reference['doc'])
-@click.option('--aoi', '-a', 'e_aoi', help=aoi['doc'])
+@click.option('--aoi', '-a', 'e_aoi', default=None, help=aoi['doc'])
 def entry(e_input_reference, e_aoi):
     
-    aoi['value'] = e_aoi
-    input_reference['value'] = e_input_reference  
+    input_reference['value'] = e_input_reference # overwrites the "default" value with the one entered in the command line
+    
+    if e_aoi is not None: aoi['value'] = e_aoi # replace with aoi entered, otherwise use default
     
     main(input_reference, aoi)
 
 def main(input_reference, aoi):
 
     set_env()
-      
-    item = get_item(os.path.join(input_reference['value'], 
-                                 'catalog.json'))
-    cat = Catalog.from_file(os.path.join(input_reference['value'], 'catalog.json')) 
     
-    asset_red, asset_nir, asset_swir16, asset_swir22 = get_assets(item)
+    item = get_item(os.path.join(input_reference['value'], 'catalog.json'))
     
+    cat = Catalog.from_file(os.path.join(input_reference['value'], 'catalog.json')) # Catalog is saved in the input_reference['value'] path (filename catalog.json)
     
+    # Extract your assets (ie input data bands)
+    asset_blue, asset_green, asset_red, asset_nir, asset_swir16, asset_swir22 = get_assets(item)
     
     vrt = '{0}.vrt'.format(item.id)
+    print('- vrt name:', vrt)
     
     ds = gdal.BuildVRT(vrt,
-                   [asset_red, asset_nir, asset_swir16, asset_swir22],
+                   [asset_blue, asset_green, asset_red, asset_nir, asset_swir16, asset_swir22],
                    srcNodata=0,
                    xRes=10, 
                    yRes=10,
                    separate=True)
-
-    ds.FlushCache()
-
-    ds = None
-
-    del(ds)
+    print('- ds loaded correctly')
     
+    ds.FlushCache() 
+    
+
+    ds = None; del(ds)
+    
+    print('- now preparing tif')
     tif = '{0}.tif'.format(item.id)
+    print('- tif name', tif)
     
+    #print(aoi['value'])
     min_lon, min_lat, max_lon, max_lat = loads(aoi['value']).bounds
+    print('-', min_lon, min_lat, max_lon, max_lat)
+    print('- check bbox of item:', item.bbox) # 106.80765, -7.32209, 107.80392, -6.324961
+    
+    # manually define new bounds to fit the temporary image
+    min_lon, min_lat, max_lon, max_lat = [106.85, -7.30, 107.80, -6.35]
+    print('-', min_lon, min_lat, max_lon, max_lat)
+    
+    os.environ['PREFIX'] = '/opt/anaconda/envs/env_vi/'
+    os.environ['PROJ_LIB'] = os.path.join(os.environ['PREFIX'], 'share/proj')
+    os.environ['GPT_BIN'] = os.path.join(os.environ['PREFIX'], 'snap/bin/gpt')
+    print('- envi updated')
     
     gdal.Translate(tif,
                    vrt,
                    outputType=gdal.GDT_Int16,
-                   projWin=[min_lon, max_lat, max_lon, min_lat],
+                   projWin=[min_lon, max_lat, max_lon, min_lat], # in the format [ulx, uly, lrx, lry]
                    projWinSRS='EPSG:4326')
     
+    print('- translate ok')
     os.remove(vrt)
-        
+    print('- tiff created successfully.\nNow load with gdal.Open')
+    
     ds = gdal.Open(tif)
     width = ds.RasterXSize
     height = ds.RasterYSize
@@ -93,44 +108,71 @@ def main(input_reference, aoi):
     input_geotransform = ds.GetGeoTransform()
     input_georef = ds.GetProjectionRef()
     
-    red = ds.GetRasterBand(1).ReadAsArray()
-    nir = ds.GetRasterBand(2).ReadAsArray()
-    swir16 = ds.GetRasterBand(3).ReadAsArray()
-    swir22 = ds.GetRasterBand(4).ReadAsArray()
+    blue = ds.GetRasterBand(1).ReadAsArray()
+    green = ds.GetRasterBand(2).ReadAsArray()
+    red = ds.GetRasterBand(3).ReadAsArray()
+    nir = ds.GetRasterBand(4).ReadAsArray()
+    swir16 = ds.GetRasterBand(5).ReadAsArray()
+    swir22 = ds.GetRasterBand(6).ReadAsArray()
     
     ds = None
-
     os.remove(tif)
-    
     del(ds)
-  
-    nbr = normalized_difference(nir, swir22)
     
-    swir22 = None
-
-    del(swir22)
-    
+    #----------------------------------------------
+    """Now calculate Spectral Indices. The list is:
+    Normalized Difference Vegetation Index (NDVI)
+    Enhanced Vegetation Index (EVI)
+    Soil Adjusted Vegetation Index (SAVI)
+    Modified Soil Adjusted Vegetation Index (MSAVI)
+    Normalized Difference Moisture Index (NDMI)
+    Normalized Burn Ratio (NBR)
+    Normalized Burn Ratio 2 (NBR2)
+    Normalized Difference Water Index
+    Normalized Difference Built-up Index
+    --> info here: https://www.usgs.gov/core-science-systems/nli/landsat/landsat-surface-reflectance-derived-spectral-indices?qt-science_support_page_related_con=0#qt-science_support_page_related_con)"""
+    stop
+    # Normalized Difference Vegetation Index (NDVI) = (NIR - R) / (NIR + R)
     ndvi = normalized_difference(nir, red)
     
-    red = None
-
-    del(red)
+    # Enhanced Vegetation Index (EVI) = G * ((NIR - R) / (NIR + C1 * R – C2 * B + L_evi))
+    #asset_evi = G*( (NIR - R) / (NIR + C1*R - C2*B + L_evi) )
+    evi = generate_evi(blue, green, red, nir, C1 = 6, C2 = 7.5, L_evi = 1) # these are values used in S2 example here: https://custom-scripts.sentinel-hub.com/sentinel-2/evi/#. 
     
-    ndwi = normalized_difference(nir, swir16)
+    # Soil Adjusted Vegetation Index (SAVI) = ((NIR - R) / (NIR + R + L_savi)) * (1 + L_savi)
+    #asset_savi = ((NIR - R) / (NIR + R + L_savi)) * (1 + L_savi)
+    savi = generate_savi(red, nir, L_savi = 0.5)
     
-    nir = swir16 = None
-
-    del(nir)
-
-    del(swir16)
+    # Modified Soil Adjusted Vegetation Index (MSAVI) = (2 * NIR + 1 – sqrt ((2 * NIR + 1)^2 – 8 * (NIR - R))) / 2
+    #asset_msavi = (2*NIR + 1 – np.sqrt((2 * NIR + 1)**2 – 8*(NIR - R))) / 2
+    msavi = generate_msavi(red, nir)
     
+    # Normalized Difference Moisture Index (NDMI) or Normalized Burn Ratio (NBR) or Normalized Difference Water Index (NDWI)
+    # NDMI or NBR or NDWI = (NIR - SWIR) / (NIR + SWIR)
+    ndmi = normalized_difference(nir, swir22)
+    nbr = normalized_difference(nir, swir22)
+    ndwi = normalized_difference(nir, swir22)
     
-    default_bands = [{'name': 'NBR',
-              'common_name': 'nbr'}, 
-             {'name': 'NDVI',
-              'common_name': 'ndvi'},
-             {'name': 'NDWI',
-              'common_name': 'ndwi'}]
+    # Normalized Burn Ratio 2 (NBR2) = (SWIR1 – SWIR2) / (SWIR1 + SWIR2)
+    nbr2 = normalized_difference(swir16, swir22)
+    
+    # Normalized Difference Built-up Index (NDBI) (SWIR - NIR) / (SWIR + NIR)
+    ndbi = normalized_difference(swir22, nir)
+    #----------------------------------------------
+    
+    # Now delete bands as not in use anymore
+    blue = green = red = nir = swir16 = swir22 = None
+    del(blue); del(green); del(red); del(nir); del(swir16); del(swir22) 
+    
+    default_bands = [{'name': 'NDVI', 'common_name': 'ndvi'},
+                     {'name': 'EVI', 'common_name': 'evi'},
+                     {'name': 'SAVI', 'common_name': 'savi'},
+                     {'name': 'MSAVI', 'common_name': 'msavi'},
+                     {'name': 'NDMI', 'common_name': 'ndmi'},
+                     {'name': 'NBR', 'common_name': 'nbr'}, 
+                     {'name': 'NDWI', 'common_name': 'ndwi'},
+                     {'name': 'NBR2', 'common_name': 'nbr2'}, 
+                     {'name': 'NDBI', 'common_name': 'ndbi'}]
     
     catalog = Catalog(id='catalog-{}'.format(item.id),
                       description='Results')
@@ -152,18 +194,18 @@ def main(input_reference, aoi):
                        datetime=item.datetime,
                        properties=item.properties)
     
-    result_item.common_metadata.set_gsd(10)
+    result_item.common_metadata.set_gsd(10) # replace '10' with 'item.properties['eo:gsd']'
     
-    eo_item = extensions.eo.EOItemExt(result_item)
+    eo_item = extensions.eo.EOItemExt(result_item) # from pystac https://pystac.readthedocs.io/en/latest/api.html
     
-    #eo_item.set_bands(bands)
+    #eo_item.set_bands(bands) # if I want to set the bands I calculated (thought I must not delete them!)
     
     #eo_item.common_metadata.set_gsd(10)
 
                      #                     result_item = EOItem(id=item_name,
                      #    geometry=item.geometry,
                      #    bbox=item.bbox,
-                     ##    datetime=item.datetime,
+                     #    datetime=item.datetime,
                      #    properties={},
                      #    bands=bands,
                      #    gsd=10, 
@@ -171,7 +213,7 @@ def main(input_reference, aoi):
                      #    instrument=item.instrument)
     bands = []
     
-    for index, veg_index in enumerate(['NBR', 'NDVI', 'NDWI']):
+    for index, veg_index in enumerate(['NDVI', 'EVI', 'SAVI', 'MSAVI', 'NDMI', 'NBR', 'NDWI', 'NBR2', 'NDBI']): # ensure these are in the same order as default_bands
 
         temp_name = '_{}_{}.tif'.format(veg_index, item.id)
         output_name = '{}_{}.tif'.format(veg_index, item.id)
@@ -192,9 +234,7 @@ def main(input_reference, aoi):
 
         sleep(5)
 
-        output = None
-
-        del(output)
+        output = None; del(output)
 
         os.makedirs(os.path.join('.', item_name),
                     exist_ok=True)
@@ -203,7 +243,7 @@ def main(input_reference, aoi):
 
         result_item.add_asset(key=veg_index.lower(),
                               asset=Asset(href='./{}'.format(output_name), 
-                                       media_type=MediaType.GEOTIFF))
+                                          media_type=MediaType.GEOTIFF))
         
         asset = result_item.get_assets()[veg_index.lower()]                                   
          
@@ -233,7 +273,8 @@ def main(input_reference, aoi):
     
     
 if __name__ == '__main__':
-    entry()
+    print('ciao')
+    #entry()
 
             
 
--- src/vegetation_index/helpers_backup.py
+++ src/vegetation_index/helpers_backup.py
@@ -4,7 +4,6 @@ from urllib.parse import urlparse
 import os
 import numpy as np
 
-
 gdal.UseExceptions()
 
 def set_env():
@@ -17,7 +16,7 @@ def set_env():
         os.environ['PROJ_LIB'] = os.path.join(os.environ['PREFIX'], 'share/proj')
 
 def fix_asset_href(uri):
-    #print('-', uri)
+
     parsed = urlparse(uri)
     
     if parsed.scheme.startswith('http'):
@@ -46,84 +45,48 @@ def get_item(catalog):
 def get_assets(item):
     
     eo_item = extensions.eo.EOItemExt(item)
-    #print(eo_item)
     
-    # Get bands
     if (eo_item.bands) is not None:
+    
         for index, band in enumerate(eo_item.bands):
-            #print(index, band, band.common_name, band.name) # eg --> 1 <Band name=B02> blue B02
-            if band.common_name in ['blue']: 
-                asset_blue = fix_asset_href(item.assets[band.name].get_absolute_href())
-            
-            if band.common_name in ['green']: 
-                asset_green = fix_asset_href(item.assets[band.name].get_absolute_href())
-            
-            if band.common_name in ['red']: 
-                asset_red = fix_asset_href(item.assets[band.name].get_absolute_href())
-            
-            if band.common_name in ['nir']: 
-                asset_nir = fix_asset_href(item.assets[band.name].get_absolute_href())
-            
-            if band.common_name in ['swir16']: 
+
+            if band.common_name in ['swir16']:
+
                 asset_swir16 = fix_asset_href(item.assets[band.name].get_absolute_href())
-            
-            if band.common_name in ['swir22']: 
+
+            if band.common_name in ['swir22']:
+
                 asset_swir22 = fix_asset_href(item.assets[band.name].get_absolute_href())
 
+            if band.common_name in ['nir']:
+
+                asset_nir = fix_asset_href(item.assets[band.name].get_absolute_href())
+
+            if band.common_name in ['red']:
+
+                asset_red = fix_asset_href(item.assets[band.name].get_absolute_href())
+
     else:
-        asset_blue = fix_asset_href(item.assets['B02'].get_absolute_href())
-        asset_green = fix_asset_href(item.assets['B03'].get_absolute_href())
+        
         asset_red = fix_asset_href(item.assets['B04'].get_absolute_href())
         asset_nir = fix_asset_href(item.assets['B08'].get_absolute_href())
         asset_swir16 = fix_asset_href(item.assets['B11'].get_absolute_href())
         asset_swir22 = fix_asset_href(item.assets['B12'].get_absolute_href())
-    
-    return asset_blue, asset_green, asset_red, asset_nir, asset_swir16, asset_swir22
 
-    
+        
+    return asset_red, asset_nir, asset_swir16, asset_swir22
+   
 def normalized_difference(band_1, band_2):
     
-    width = np.shape(band_1.shape)[0] # is this correct?!? or just np.shape(band1)[0] probably?
+    width = np.shape(band_1.shape)[0]
     height = np.shape(band_1)[1]
 
     norm_diff = np.zeros((height, width), dtype=np.uint)
+
     norm_diff = 10000 * ((band_1 - band_2) / (band_1 + band_2))
     
     return norm_diff
-
-"""
-def generate_evi(blue, green, red, nir, C1=6, C2=7.5, L_evi=1):
-    #C1 = 6; C2 = 7.5; L_evi = 1 # these are values used in S2 example here: https://custom-scripts.sentinel-hub.com/sentinel-2/evi/#. 
     
-    width = np.shape(blue.shape)[0] # check shape. not sure this is correct.
-    height = np.shape(blue)[1]
-
-    evi = np.zeros((height, width), dtype=np.uint)
-    
-    evi = green*( (nir - red) / (nir + C1*red - C2*blue + L_evi) )
-    
-    return evi
-    
-def generate_savi(red, nir, L_savi = 0.5): 
-    
-    width = np.shape(red.shape)[0] # check shape. not sure this is correct.
-    height = np.shape(red)[1]
-
-    savi = np.zeros((height, width), dtype=np.uint)
-    
-    savi = ((nir - red) / (nir + red + L_savi)) * (1 + L_savi)
-    return savi
-
-def generate_msavi(red, nir):
-    
-    width = np.shape(red.shape)[0] # check shape. not sure this is correct.
-    height = np.shape(red)[1]
-
-    msavi = np.zeros((height, width), dtype=np.uint)
-    
-    msavi = (2*nir + 1 – np.sqrt((2 * nir + 1)**2 – 8*(nir - red))) / 2
-    return msavi
-"""
 
 def cog(input_tif, output_tif,no_data=None):
     
