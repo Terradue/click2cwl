3b06b29753978fca3cea683328e3bf25d1702732
fabrice.brito@terradue.com <fabrice.brito@terradue.com>
2021-01-12 15:09:09 +0000

    tidy...

---
+++ conda.recipe/run_test.py
@@ -0,0 +1,5 @@
+
+import click
+import click2cwl
+from click2cwl import dump
+from click2cwl import Click2CWL
---
+++ src/click2cwl/__init__.py
@@ -0,0 +1,5 @@
+#__import__("pkg_resources").declare_namespace(__name__)
+from .entry import Click2CWL
+from .cwlexport import CWLExport
+from .paramexport import ParamExport
+from .entry import dump
