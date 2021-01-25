b9ede4473faa7e318461dca9a183143b99c75c80
fabrice.brito@terradue.com <fabrice.brito@terradue.com>
2021-01-25 16:22:22 +0000

    update

---
+++ src/click2cwl/metadata.py
@@ -0,0 +1,29 @@
+from types import SimpleNamespace
+
+class WorkflowMetadata(SimpleNamespace):
+    
+    def __init__(self, **kwargs):
+
+        self._fields = {**kwargs}
+
+        return super().__init__(**self._fields)
+
+    def to_dict(self):
+        
+        metadata = {}
+        metadata['$namespaces'] = {'s': 'https://schema.org/'}
+        metadata['schemas'] = ['http://schema.org/version/9.0/schemaorg-current-http.rdf']
+        
+        if 'author' in list(self._fields.keys()):
+            
+            metadata['s:author'] = [{'class': 's:Person', 's:name': self.author}] 
+            
+        if 'organization' in list(self._fields.keys()):
+            
+            metadata['s:organization'] = [{'class': 's:Organization', 's:url': self.organization}] 
+
+        if 'version' in list(self._fields.keys()):
+        
+            metadata['s:softwareVersion'] = self.version
+        
+        return metadata
